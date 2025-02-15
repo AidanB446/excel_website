de66e6373eb47d96cf4d0a74d8df65b1c49f36ba
Aidan Bruner <Aidanbruner789@gmail.com>
2025-02-15 18:39:55 -0500

    asdfasdf

--- excel_site/app/math/page.js
+++ excel_site/app/math/page.js
@@ -1,20 +1,31 @@
 import styles from "./page.module.css";
-
-
-
+import invsq from "./public/invsq.png";
+import shootAlien from "./public/shootalien.webp";
 
 export default function Home() {
   return (
 	<div>
-
 		<title>Math</title>
-
 	  	<div className={styles.body}>
+			<center><h1>Our Math Research</h1></center>
+			
+			<div className={styles.invsqrt}>
+				<center>
+					<p>In order to design a game we needed to study what formulas were used in other games, and how forumlas affected the quality of games. After doing some research we found one of the most popular and influential algorithms in gaming was the Fast Inverse Sqaure algorithm used in the first person shooter quake. This algorithm utilizes bit shifting in order to calculate values representing the inverse sqaure law in physics, a law that demonstrates light levels based upon distance. By quake utilizing this algorithm it made their game more graphically advanced.</p> 
+					<br/><br/><br/>
+	  				<div><img src={invsq.src}></img></div>
+					<br/>	
+					<br/>	
+					<br/>	
+					<br/>	
+					<h2>How our Research Influenced our Game</h2>	
+					<p>From this algorithm we can derive 1/sqrt(x) as the value they are calculating for. We can utilize this function as inspiration for a exponential point counting system for the player. We created a shooter game attraction where you are the player fighting off an alien invasion. The point system we designed will drop points rapidly when the player is hit, and will slowly increase as the player eliminates the alien threats. Creating a challenging memoriable player experience.</p>
 
+	  				<div><img src={shootAlien.src}></img></div>
+	  			</center>
 
+	  		</div>
 	  	</div>		
-
-
 	</div>	
   );
 }
--- excel_site/app/science/page.js
+++ excel_site/app/science/page.js
@@ -1,9 +1,33 @@
+import {FLIGHT_HEADERS} from "next/dist/client/components/app-router-headers";
 import styles from "./page.module.css";
+import gforce from "./public/Tolerance-to-G-z-Acceleration.png";
 
 export default function Home() {
   return (
 
 	<div>
+		<title>Science</title>
+
+	  	<div className={styles.body}>
+			<center>
+				<h1>Anatomy RollerCoaster, Spiral Out</h1>
+				<p>A staple of all theme parks is a RollerCoaster. So what is better than a RollerCoaster with a track modeled after human skeletal Anatomy. As presented in our model, this RollerCoaster takes the guest on a thrilling ride. <br/><br/> In order to make this ride possible we needed to study the effects of GForces on the Human Body.
+	  Most coasters are pulled by motorized chain tracks and are designed to manage 	G-force on the human body. Most human bodies can handle 1-5 G’s tracks are designed to manage the G-force on the body. G force is the measure of acceleration felt by the human body through gravity. </p>
+				
+	  			<div style={{"width": "33%"}}><img src={gforce.src}></img></div>
+				<br/>
+				<br/>
+	  			<p>With this research in mind we made sure to keep our coaster under 2.3 Gz to ensure safety of our riders. </p>	
+
+	  		</center>
+			
+
+
+	  	</div>	
+
+
+
+
 	</div>	
 
   );
