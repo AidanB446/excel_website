ce951c8c71de2facf8f476ab4a15336ab54fc685
Aidan Bruner <Aidanbruner789@gmail.com>
2025-02-17 21:56:21 -0500

    adding video to site

--- excel_site/app/commercial/page.js
+++ excel_site/app/commercial/page.js
@@ -2,9 +2,12 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-
-	<div>
+	<div className={styles.body}>
+		<center>
+			<video controls preload="none">
+				<source src="/lv_0_20250217210902.mp4" type="video/mp4"/>
+			</video>
+	  	</center>
 	</div>	
-
   );
 }
